fc70a4867ea48a7bacbb87f942eac0b18038d706
Felipe Barcellos <felipe.cellos@hotmail.com>
2024-10-15 01:39:06 -0300

    removed unused imports

--- webscrap/ScrapperPichau.py
+++ webscrap/ScrapperPichau.py
@@ -1,10 +1,6 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from time import sleep
 from loguru import logger
-from util.constants import CHROME_DRIVER_PATH, PRECOS_PATH
 from webscrap.Scrapper import Scrapper
 
 
